5be5179dffbe3079ef8c4294e6519f56bb651627
Andrei Kulakov <krugliy@gmail.com>
2010-09-24 00:53:09 +0300

    Modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,4 @@
 set :use_sudo, false
-
 default_run_options[:pty] = true
 
 set :application, "rails3_fcgi"
@@ -15,7 +14,12 @@ role :app, domain  # This may be the same as your `Web` server
 role :db, domain, :primary => true  # This is where Rails migrations will run
 
 ssh_options[:forward_agent] = true
-
 set :brach, "master"
-
 set :deploy_via, :remote_cache
+
+deploy.task :db_link do
+  run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
+end
+
+after "deploy:update_code", "deploy:db_link"
