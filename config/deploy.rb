39b4b7f266f54ed2c273a8ff8500fa251b7d8f14
Andrei Kulakov <krugliy@gmail.com>
2010-09-24 01:13:40 +0300

    Modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,9 +1,6 @@
-set :use_sudo, false
-default_run_options[:pty] = true
-
 set :application, "rails3_fcgi"
-set :domain,      "#{application}.lan"
 set :user,        "krg"  # The server's user for deploys
+set :domain,      "#{application}.lan"
 set :repository,  "git@github.com:dre3k/#{application}.git"
 set :deploy_to,   "/home/#{user}/www/#{application}"
 
@@ -13,13 +10,22 @@ role :web, domain  # Your HTTP server, Apache/etc
 role :app, domain  # This may be the same as your `Web` server
 role :db, domain, :primary => true  # This is where Rails migrations will run
 
+default_run_options[:pty] = true
+set :use_sudo, false
 ssh_options[:forward_agent] = true
 set :brach, "master"
 set :deploy_via, :remote_cache
+set :scm_verbose, true
 
 deploy.task :db_link do
   run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
   run "ln -nfs #{shared_path}/db/production.sqlite3 #{release_path}/db/production.sqlite3"
 end
-
 after "deploy:update_code", "deploy:db_link"
+
+namespace :deploy do
+  task :restart do
+    run "#{sudo} killall dispatch.fcgi"
+    run "wget #{domain} --spider -O  -"
+  end
+end
