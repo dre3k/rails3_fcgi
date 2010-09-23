c431939b6cb6e143a8e99c767599acf7ef2e7225
Andrei Kulakov <krugliy@gmail.com>
2010-09-23 23:43:28 +0300

    Modify deploy.rb

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,19 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+default_run_options[:pty] = true
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :application, "rails3_fcgi"
+set :domain,      "#{application}.lan"
+set :user,        "krg"  # The server's user for deploys
+set :repository,  "git@github.com:dre3k/#{application}.git"
+set :deploy_to,   "ssh://#{user}@#{domain}/home/#{user}/www/#{application}"
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+set :scm, :git
 
-# If you are using Passenger mod_rails uncomment this:
-# if you're still using the script/reapear helper you will need
-# these http://github.com/rails/irs_process_scripts
+role :web, domain  # Your HTTP server, Apache/etc
+role :app, domain  # This may be the same as your `Web` server
+role :db, domain, :primary => true  # This is where Rails migrations will run
 
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+ssh_options[:forward_agent] = true
+
+set :brach, "master"
+
+set :deploy_via, :remote_cache
